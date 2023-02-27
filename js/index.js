4dd0a8c43292958835d2c9dd8dff0841426e84eb
unknown <emmanuelmanalo707@gmail.com>
2023-02-27 11:21:29 +0800

    header js

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const toggleMenu = document.querySelector(".toggle__menu");
+const headerBot = document.querySelector(".header__nav");
+
+toggleMenu.addEventListener("click", () => {
+  toggleMenu.classList.toggle("open");
+  headerBot.classList.toggle("open");
+});
